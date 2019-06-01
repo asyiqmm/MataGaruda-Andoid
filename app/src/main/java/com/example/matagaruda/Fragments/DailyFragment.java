96ea67784975cb31ed85b57ad693002fb59bf99a
asyiqmm <asyiqmm@gmail.com>
2019-06-01 12:54:57 +0700

    all layout update2

---
+++ app/src/main/java/com/example/matagaruda/Fragments/DailyFragment.java
@@ -0,0 +1,17 @@
+package com.example.matagaruda.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.matagaruda.R;
+
+public class DailyFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_daily, container, false);
+    }
+}
